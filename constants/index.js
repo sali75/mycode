77d5d787e5390e987e7ed2658898301a8461078c
sali75 <sali75911@gmail.com>
2023-07-20 11:28:53 +0330

    Update index.js
    update contract

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x8435db29ebf6e7d4981d787167a7b943e2147ed0";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
